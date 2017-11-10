108989349202501a43402ee25f956465ec0238f0
Preethika Ashokumar <babbuashokumar@gmail.com>
2017-11-10 15:35:06 -0800

    Update Using_NULL.sql

--- Using_NULL.sql
+++ Using_NULL.sql
@@ -3,24 +3,57 @@ Tables to use for the Queries
 teacher
 id|dept|name|phone|mobile
 --|--|--|--|--|--|
-101	 1	    Shrivell	   2753	  07986 555 1234
-102	 1	    Throd	       2754	  07122 555 1920
-103	 1	    Splint	     2293	
-104		      Spiregrain	 3287	
-105	 2	    Cutflower	   3212	  07996 555 6574
-106		      Deadyawn	   3345	
+101	 1	Shrivell	   2753	  07986 555 1234
+102	 1	Throd	       2754	  07122 555 1920
+103	 1	Splint	     2293	
+104		  Spiregrain	 3287	
+105	 2	Cutflower	   3212	  07996 555 6574
+106		  Deadyawn	   3345	
 ...
 
 dept
 id|name
----- |----
+--|----
 1|Computing
 2|Design
 3|Engineering
 ...
 
--1.List the teachers who have NULL for their department.
+--1.List the teachers who have NULL for their department.
+SELECT name 
+FROM teacher 
+WHERE dept IS NULL
 
-Query: Select name from teacher where dept is NULL
+--2.Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+SELECT teacher.name, dept.name
+FROM teacher 
+INNER JOIN dept
+ON (teacher.dept=dept.id)
 
-2.
+--3.Use a different JOIN so that all teachers are listed.
+SELECT teacher.name,dept.name 
+FROM teacher 
+Left JOIN dept 
+ON(teacher.dept=dept.id)
+
+--4.Use a different JOIN so that all departments are listed.
+SELECT teacher.name,dept.name 
+FROM teacher 
+Right JOIN dept 
+ON(teacher.dept=dept.id)
+
+--5.Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+SELECT teacher.name,COALESCE(mobile, '07986 444 2266')
+FROM teacher
+
+--6.Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE(dept.name,'none') 
+FROM teacher l
+LEFT JOIN dept 
+ON (teacher.dept=dept.id)
+
+--7.Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(teacher.id),COUNT(teacher.mobile) 
+FROM teacher
+
+--8.
