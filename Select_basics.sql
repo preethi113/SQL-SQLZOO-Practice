b0da482c68ae7f6f7f5d3d687bbdadac94c10893
Preethika Ashokumar <babbuashokumar@gmail.com>
2017-11-10 16:51:11 -0800

    Update Select_basics.sql

--- Select_basics.sql
+++ Select_basics.sql
@@ -1,4 +1,4 @@
-Refer 
+Refer select_basics_table.png for table image.
 
 --1.The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
 --  Modify it to show the population of Germany
