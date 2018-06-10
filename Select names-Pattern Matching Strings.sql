1b55f85748ebc9eb558430bdcaf338982813356c
Preethika Ashokumar <babbuashokumar@gmail.com>
2018-06-10 16:17:18 -0700

    Update Select names-Pattern Matching Strings.sql

--- Select names-Pattern Matching Strings.sql
+++ Select names-Pattern Matching Strings.sql
@@ -1,1 +1,5 @@
-Refer select_names_patternmatchingtable.png
+Refer select_names_patternmatching_table.png
+
+1. You can use "WHERE name LIKE 'B%'" to find the countries that starts with "B"
+   The % is a wildcard it can match any characters
+   
