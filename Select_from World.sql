46ee72771554a8d7877a70e9df737a22e7a04090
Preethika Ashokumar <babbuashokumar@gmail.com>
2018-06-11 12:51:00 -0700

    Update Select_from World.sql

--- Select_from World.sql
+++ Select_from World.sql
@@ -10,3 +10,24 @@ WHERE population >= 200000000
 
 --3.Give the name and the per capita GDP for those countries with a population of at least 200 million.
 --per capita GDP is the GDP divided by the population GDP/population.
+SELECT name, gdp/population 
+FROM world 
+WHERE population>200000000;
+
+--4.Show the name and population in millions for the countries of the continent 'South America'. 
+--Divide the population by 1000000 to get population in millions.
+SELECT name,population/1000000 
+FROM world 
+WHERE continent='South America'
+
+--5.Show the name and population for France, Germany, Italy
+SELECT name,population 
+FROM world 
+WHERE name IN ('France','Germany','Italy')
+
+--6.Show the countries which have a name that includes the word 'United'.
+SELECT name 
+FROM world 
+WHERE name LIKE '%United%'
+
+--7.
