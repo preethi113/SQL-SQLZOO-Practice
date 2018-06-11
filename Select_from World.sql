cbef2e62e4f7f94ba94f7e8bf308a064ec5e2e7c
Preethika Ashokumar <babbuashokumar@gmail.com>
2018-06-11 14:59:34 -0700

    Update Select_from World.sql

--- Select_from World.sql
+++ Select_from World.sql
@@ -30,4 +30,19 @@ SELECT name
 FROM world 
 WHERE name LIKE '%United%'
 
---7.
+--7.Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+--Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population,area 
+FROM world 
+WHERE area>3000000 OR population>250000000
+
+--8.Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
+--Australia has a big area but a small population, it should be included.
+--Indonesia has a big population but a small area, it should be included.
+--China has a big population and big area, it should be excluded.
+--United Kingdom has a small population and a small area, it should be excluded.
+SELECT name, population,area 
+FROM world 
+WHERE  area>3000000 XOR population>250000000
+
+--9.
