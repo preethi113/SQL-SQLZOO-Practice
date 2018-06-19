781ac9068bf7ed69f8b3123a6cfb0e28c6308125
Preethika Ashokumar <babbuashokumar@gmail.com>
2018-06-18 22:47:08 -0700

    Update Select_from_Nobel.sql

--- Select_from_Nobel.sql
+++ Select_from_Nobel.sql
@@ -42,6 +42,11 @@ SELECT winner
 FROM nobel 
 WHERE winner LIKE 'John%'
 
---8.
+--8.Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT *
+FROM nobel
+WHERE (subject = "Physics" AND yr = '1980') OR (subject = 'Chemistry' AND yr = 1984)
+
+--9.
                   
                   
